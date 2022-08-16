24ee378adcddcf40e3907ae5505e0db7eaddb4f6
Suresh Kumar <suresh.kumar@selfiestyler.com>
2022-08-16 23:27:25 +0500

    Problem find the node exists in tree or not

---
+++ BinaryTree/treeinclude.js
@@ -0,0 +1,44 @@
+// Problem : Find the target in the tree  like e exists in the tree or not 
+/*   a
+    /\
+  b     c
+ /\     /
+ d e    f
+ */
+
+ class Node{
+    constructor(val){
+        this.val    = val;
+        this.left   = null;
+        this.right  = null;
+    }    
+ }
+
+ let a = new Node(a);
+ let b = new Node(b);
+ let c = new Node(c);
+ let d = new Node(d);
+ let e = new Node(e);
+ let f = new Node(f);
+
+ a.left = b;
+ a.right = c
+ b.left = d;
+ b.right = e;
+ c.left = f;
+
+ const treeIncludes = (root,target) =>{
+    if(root ===null) return [];
+    let queue = [root];
+
+    while(queue.length >0){
+        let current = queue.shift();
+        if(current.val === target) return true;
+        if(current.left) queue.push(current.left);
+        if(current.right) queue.push(current.right);
+    }
+    return false;
+
+ }
+ console.log(treeIncludes(a,"b"));
+
